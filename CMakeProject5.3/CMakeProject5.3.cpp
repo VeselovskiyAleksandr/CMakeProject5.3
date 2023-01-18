efe7ec630525ce7fd528bc3bb1955b3be3c3c436
VeselovskiyAleksandr <79366865+VeselovskiyAleksandr@users.noreply.github.com>
2023-01-18 11:44:46 +0300

    Добавьте файлы проекта.

---
+++ CMakeProject5.3/CMakeProject5.3.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject5.3.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include "CMakeProject5.3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
